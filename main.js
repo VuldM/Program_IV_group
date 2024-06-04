d0cf4cb4e31b38b5d9ccd2e8e4d2df8b143e6d4f
VuldM <aniairina@gmail.com>
2024-06-04 11:27:26 +0300

    added new files json

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const mainTheme_1 = document.querySelector(".main__theme_1_btn");
 const mainTheme_2 = document.querySelector(".main__theme_2_btn");
 const mainTheme_3 = document.querySelector(".main__theme_3_btn");
+const mainTheme_4 = document.querySelector(".main__theme_4_btn");
+const mainTheme_5 = document.querySelector(".main__theme_5_btn");
+const mainTheme_6 = document.querySelector(".main__theme_6_btn");
+const mainTheme_7 = document.querySelector(".main__theme_7_btn");
+const mainTheme_8 = document.querySelector(".main__theme_8_btn");
+const mainTheme_9 = document.querySelector(".main__theme_9_btn");
 
 mainTheme_1.addEventListener("click", function (e) {
   window.location = "./theme_1/theme_1.html";
@@ -10,4 +16,22 @@ mainTheme_2.addEventListener("click", function (e) {
 });
 mainTheme_3.addEventListener("click", function (e) {
   window.location = "./theme_3/theme_3.html";
+});
+mainTheme_4.addEventListener("click", function (e) {
+  window.location = "./theme_4/theme_4.html";
+});
+mainTheme_5.addEventListener("click", function (e) {
+  window.location = "./theme_5/theme_5.html";
+});
+mainTheme_6.addEventListener("click", function (e) {
+  window.location = "./theme_6/theme_6.html";
+});
+mainTheme_7.addEventListener("click", function (e) {
+  window.location = "./theme_7/theme_7.html";
+});
+mainTheme_8.addEventListener("click", function (e) {
+  window.location = "./theme_8/theme_8.html";
+});
+mainTheme_9.addEventListener("click", function (e) {
+  window.location = "./theme_9/theme_9.html";
 });
